741b7aefc7a0a5bff4aebed7e6f803b6c42fce52
Karim Naaji <karim.naaji@gmail.com>
2016-01-03 22:40:57 +0100

    Add renderer

--- src/cmdline.c
+++ src/cmdline.c
@@ -16,6 +16,6 @@ int main(int argc, const char **argv) {
     flag_int(&tileZ, "tilez", "Tile Z");
     flag_int(&sizehint, "sizehint", "Controls resolution of atlas");
     flag_int(&nsamples, "nsamples", "Quality of ambient occlusion");
-    flag_parse(argc, argv, "v" "0.1.0", 1);
+    flag_parse(argc, argv, "v" "0.1.0", 0);
     return objexport(tileX, tileY, tileZ, splitMeshes, sizehint, nsamples);
 }
--- src/objexport.cpp
+++ src/objexport.cpp
@@ -170,16 +170,13 @@ bool saveOBJ(std::string _outputOBJ, bool _splitMeshes, const std::vector<Polygo
                         << vertex.normal.z << "\n";
                 }
                 for (int i = 0; i < mesh.indices.size(); i += 3) {
-                    file << "f " << mesh.indices[i] + indexOffset + 1 << "/"
-                        << mesh.indices[i] + indexOffset + 1 << "/"
+                    file << "f " << mesh.indices[i] + indexOffset + 1 << "//"
                         << mesh.indices[i] + indexOffset + 1;
                     file << " ";
-                    file << mesh.indices[i+1] + indexOffset + 1 << "/"
-                        << mesh.indices[i+1] + indexOffset + 1 << "/"
+                    file << mesh.indices[i+1] + indexOffset + 1 << "//"
                         << mesh.indices[i+1] + indexOffset + 1;
                     file << " ";
-                    file << mesh.indices[i+2] + indexOffset + 1 << "/"
-                        << mesh.indices[i+2] + indexOffset + 1 << "/"
+                    file << mesh.indices[i+2] + indexOffset + 1 << "//"
                         << mesh.indices[i+2] + indexOffset + 1<< "\n";
                 }
                 file << "\n";
@@ -203,16 +200,13 @@ bool saveOBJ(std::string _outputOBJ, bool _splitMeshes, const std::vector<Polygo
             }
             for (const PolygonMesh& mesh : _meshes) {
                 for (int i = 0; i < mesh.indices.size(); i += 3) {
-                    file << "f " << mesh.indices[i] + indexOffset + 1 << "/"
-                        << mesh.indices[i] + indexOffset + 1 << "/"
+                    file << "f " << mesh.indices[i] + indexOffset + 1 << "//"
                         << mesh.indices[i] + indexOffset + 1;
                     file << " ";
-                    file << mesh.indices[i+1] + indexOffset + 1 << "/"
-                        << mesh.indices[i+1] + indexOffset + 1 << "/"
+                    file << mesh.indices[i+1] + indexOffset + 1 << "//"
                         << mesh.indices[i+1] + indexOffset + 1;
                     file << " ";
-                    file << mesh.indices[i+2] + indexOffset + 1 << "/"
-                        << mesh.indices[i+2] + indexOffset + 1 << "/"
+                    file << mesh.indices[i+2] + indexOffset + 1 << "//"
                         << mesh.indices[i+2] + indexOffset + 1<< "\n";
                 }
                 indexOffset += mesh.vertices.size();
@@ -225,7 +219,6 @@ bool saveOBJ(std::string _outputOBJ, bool _splitMeshes, const std::vector<Polygo
 }
 
 int objexport(int _tileX, int _tileY, int _tileZ, bool _splitMeshes, int _sizehint, int _nsamples) {
-{
     std::string apiKey = "vector-tiles-qVaBcRA";
     std::string url = "http://vector.mapzen.com/osm/all/"
         + std::to_string(_tileZ) + "/"
@@ -249,7 +242,7 @@ int objexport(int _tileX, int _tileY, int _tileZ, bool _splitMeshes, int _sizehi
 
     std::vector<PolygonMesh> meshes;
     for (auto layer : data->layers) {
-        if (layer.name == "buildings" || layer.name == "landuse") {
+        //if (layer.name == "buildings" || layer.name == "landuse") {
             for (auto feature : layer.features) {
                 auto itHeight = feature.props.numericProps.find(key_height);
                 auto itMinHeight = feature.props.numericProps.find(key_min_height);
@@ -271,7 +264,7 @@ int objexport(int _tileX, int _tileY, int _tileZ, bool _splitMeshes, int _sizehi
                 }
                 meshes.push_back(mesh);
             }
-        }
+        //}
     }
 
     std::string outFile = std::to_string(_tileX) + "." + std::to_string(_tileY)
